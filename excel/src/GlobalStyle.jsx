e5714e6df8de4b12b93343ba27a4cd9ab2d037e2
nastar <an1413@hs.ac.kr>
2023-09-12 15:11:51 +0900

    Style: 전역스타일링 추가 a11y

--- excel/src/GlobalStyle.jsx
+++ excel/src/GlobalStyle.jsx
@@ -27,6 +27,32 @@ const GlobalStyle = createGlobalStyle`
       src: url(https://fonts.gstatic.com/s/nunito/v25/XRXV3I6Li01BKofIOOaBXso.woff2) format('woff2');
       unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
     }
+
+        .a11y-hidden {
+    clip: rect(1px, 1px, 1px, 1px);
+    clip-path: inset(50%);
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    }
+
+    button {
+        font-family: "Spoqa Han Sans Neo", "sans-serif";
+        border: none;
+        padding: 0;
+        cursor: pointer;
+        :disabled {
+            cursor: default;
+        }
+    }
+
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
 `;
 
 export default GlobalStyle;
