9be2c6b79b937b0a8828d4f5c19a3a2a5b7bb36f
nastar <an1413@hs.ac.kr>
2023-09-12 14:39:49 +0900

    Feat: Header 컴포넌틑 작성 완료

---
+++ excel/src/component/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from "styled-components";
+
+export default function Header() {
+  return (
+    <HeaderTitle>Excel Function Study with Simulator</HeaderTitle>
+  )
+}
+
+const HeaderTitle = styled.h1`
+  color: white;
+  text-align: center;
+  margin-top: 50px;
+  font-family: 'Nunito';
+  font-size: 85px;
+`
+
+
