42a1bbe90e73e29500f1130d2754e06faa0867e6
nastar <an1413@hs.ac.kr>
2023-09-12 14:14:01 +0900

    Feat: GlobalStyles 셋팅

--- excel/src/App.js
+++ excel/src/App.js
@@ -1,9 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './component/common/Footer';
+import GlobalStyle from './GlobalStyle';
 
 
 function App() {
   return (
     <>
+    <GlobalStyle/>
      <div className="container">
       <div className="row">
         <div className="col col-md-4 col-lg-8">adsds</div>
@@ -11,6 +14,7 @@ function App() {
         <div className="col md-4 col-lg-4">adsddds</div>
         <div className="col md-4 col-lg-4">adsddds</div>
         <div className="col md-4 col-lg-4">adsddds</div>
+        <Footer/>
       </div>
       </div>
     </>
