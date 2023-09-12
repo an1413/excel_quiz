044115746686f871a84c38fec07cec644b9b88d1
nastar <an1413@hs.ac.kr>
2023-09-12 15:12:36 +0900

    Feat: HomeFeed, Header 컴포넌트 작동 확인

--- excel/src/App.js
+++ excel/src/App.js
@@ -1,6 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './component/common/Footer';
 import GlobalStyle from './GlobalStyle';
+import Header from './component/Header';
+import HomeFeed from './pages/HomeFeed/HomeFeed';
 
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
     <GlobalStyle/>
      <div className="container">
       <div className="row">
+        <HomeFeed/>
         <div className="col col-md-4 col-lg-8">adsds</div>
         <div className="col md-4 col-lg-4">adsddds</div>
         <div className="col md-4 col-lg-4">adsddds</div>
