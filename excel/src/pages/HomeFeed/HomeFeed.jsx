cd52c7c581bd314cd6928d34854fec12c9a2b54b
nastar <an1413@hs.ac.kr>
2023-09-12 15:11:31 +0900

    Feat: HomeFeed.jsx 작업추가

--- excel/src/pages/HomeFeed/HomeFeed.jsx
+++ excel/src/pages/HomeFeed/HomeFeed.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
+import Header from '../../component/Header'
+import { Img, LevelButton, LevelContainer, LevelImg, LevelName, Main } from './homeFeed.style'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import newbie from "../../img/newbie3.png"
 
 export default function HomeFeed() {
   return (
-    <div>HomeFeed</div>
+    <>
+      <Header/>
+      <div className='container'>
+        <div className='row'>
+          <LevelContainer className='col col-md-4 col-lg-4'>
+            <LevelImg>
+              <Img
+                aria-label="채팅방 이동버튼"
+                src={
+                  newbie
+                }
+                alt="뉴비"
+              />
+            </LevelImg>
+            <LevelName>초급</LevelName>
+            <LevelButton/>
+          </LevelContainer>
+
+          <LevelContainer className='col col-md-4 col-lg-4'>
+            <LevelImg/>
+            <LevelName>중급</LevelName>
+            <LevelButton/>
+          </LevelContainer>
+
+          <LevelContainer className='col col-md-4 col-lg-4'>
+            <LevelImg/>dd
+            <LevelName>고급</LevelName>
+            <LevelButton/>
+          </LevelContainer>
+        </div>
+      </div>
+    </>
   )
 }
---
+++ excel/src/pages/HomeFeed/homeFeed.style.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const LevelContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`
+
+export const LevelImg = styled.div`
+`
+
+export const LevelName = styled.div`
+
+`
+
+export const LevelButton = styled.div`
+`
+
+export const Img = styled.img`
+  width: 240px;
+  height: 240px;
+`;
