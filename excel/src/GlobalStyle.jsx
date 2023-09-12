749df8db10f0d92f2572c56bf6ca9db151e87105
nastar <an1413@hs.ac.kr>
2023-09-12 14:18:53 +0900

    Feat: GmarketSans폰트 적용

--- excel/src/GlobalStyle.jsx
+++ excel/src/GlobalStyle.jsx
@@ -6,9 +6,24 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family: "Helvetica", "Arial", sans-serif;
+    font-family: GmarketSansMedium, "Arial", sans-serif;
     line-height: 1.5;
   }
+
+    @font-face {
+    font-family: 'GmarketSansMedium';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+    }
+    @font-face {
+      font-family: 'Nunito';
+      font-style: normal;
+      font-weight: 900;
+      font-display: swap;
+      src: url(https://fonts.gstatic.com/s/nunito/v25/XRXV3I6Li01BKofIOOaBXso.woff2) format('woff2');
+      unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
+    }
 `;
 
 export default GlobalStyle;
