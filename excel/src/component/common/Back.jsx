780af4d416683ea07a07410751629c098ef2f3fe
nastar <an1413@hs.ac.kr>
2023-09-14 12:56:02 +0900

    Feat: BackButton 공통컴포넌트 작성

---
+++ excel/src/component/common/Back.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from "styled-components";
+
+export default function Back() {
+  return (
+    <BackButton>Back</BackButton>
+  )
+}
+
+const BackButton = styled.button`
+  width: 24px;
+  height: 24px;
+`
