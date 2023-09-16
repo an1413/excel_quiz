c8e3cd03c56af4d6473819c45923e85e2a7113a4
nastar <an1413@hs.ac.kr>
2023-09-16 14:38:52 +0900

    Feat: Router추가

--- excel/src/routes/Router.jsx
+++ excel/src/routes/Router.jsx
@@ -11,6 +11,8 @@ import Expert from "../pages/Expert/Expert";
 import Dictionary from "../pages/Dictionary/Dictionary";
 import Beginner from "../pages/Beginner/Beginner";
 import Intern from "../pages/Intern/Intern";
+import Completion from '../pages/Completion/Completion';
+import TestRandom from '../pages/TestRandom/TestRandom';
 
 export default function Router() {
   return (
@@ -18,10 +20,19 @@ export default function Router() {
       <Routes>
         <Route path="/" element={<HomeFeed/>}/>
         <Route path="/dictionary" element={<Dictionary/>}/>
-        <Route path="/beginner" element={<Beginner/>}></Route>
-        <Route path="/intern" element={<Intern/>}></Route>
-        <Route path="/expert" element={<Expert/>}></Route>
-        
+        <Route path="/beginner/*" element={<Outlet/>}>
+          <Route path="" element={<Beginner />} />
+          <Route path='completion/' element={<Completion level="Beginner"/> }/>
+        </Route>
+        <Route path="/intern/*" element={<Outlet/>}>
+          <Route path="" element={<Intern />} />
+          <Route path='completion/' element={<Completion level="Intern"/>}/>
+        </Route>
+        <Route path="/expert/*" element={<Outlet/>}>
+          <Route path="" element={<Expert />} />
+          <Route path='completion/' element={<Completion level="Expert"/>}/>
+        </Route>
+        <Route path='/test/*' element={<TestRandom/>}/>
       </Routes>
     </BrowserRouter>
   )
