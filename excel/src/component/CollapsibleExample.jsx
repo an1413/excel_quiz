2337331c7cc5cea26b5f4ff8a1265fb4e953ddc1
nastar <an1413@hs.ac.kr>
2023-09-22 00:25:40 +0900

    Feat: CollapsibleExample 컴포넌트 추가

---
+++ excel/src/component/CollapsibleExample.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function CollapsibleExample() {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">엑셀문제</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <NavDropdown title="Question" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="/beginner">초급</NavDropdown.Item>
+              <NavDropdown.Item href="/intern">중급</NavDropdown.Item>
+              <NavDropdown.Item href="/expert">고급</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/test">
+                실전테스트
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="/dictionary">함수사전</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="/signup">회원가입</Nav.Link>
+            <Nav.Link href="/login">로그인</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default CollapsibleExample;
