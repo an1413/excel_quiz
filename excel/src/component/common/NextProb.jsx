0fec2e5f39dacf19bdda235f2b10e519449cd5a4
nastar <an1413@hs.ac.kr>
2023-11-06 02:05:37 +0900

    Feat: 이전, 다음버튼 컴포넌트

---
+++ excel/src/component/common/NextProb.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from "styled-components";
+import Swal from "sweetalert2";
+
+export default function NextProb({ onNext }) {
+  const handle_next = () => {
+        onNext(); // 다음 스테이지로 이동
+  };
+
+  return <NextButton onClick={handle_next}>다음 문제</NextButton>;
+}
+
+const NextButton = styled.button`
+  font-size: 15px;
+  background-color: #000;
+  color: #fff;
+  font-family: 'GmarketSansMedium';
+  width: 82px;
+  border-radius: 4px;
+  padding: 2px;
+  &:hover {
+    background-color: #4B504B;
+    color: #fff;
+  }
+`;
---
+++ excel/src/component/common/PrevProb.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from "styled-components";
+import Swal from "sweetalert2";
+
+export default function PrevProb({ onPrev }) {
+  const handle_prev = () => {
+        onPrev(); // 이전 스테이지로 이동
+  };
+
+  return <PrevButton onClick={handle_prev}>이전 문제</PrevButton>;
+}
+
+const PrevButton = styled.button`
+  font-size: 15px;
+  background-color: #000;
+  color: #fff;
+  font-family: 'GmarketSansMedium';
+  width: 82px;
+  border-radius: 4px;
+  padding: 2px;
+  &:hover {
+    background-color: #4B504B;
+    color: #fff;
+  }
+`;
