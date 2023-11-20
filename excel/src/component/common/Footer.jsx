5966a43faa1f5c801b1b42597ec8476835526888
nastar <an1413@hs.ac.kr>
2023-11-20 14:10:12 +0900

    Feat: Footer 컴포넌트초안 제작

--- excel/src/component/common/Footer.jsx
+++ excel/src/component/common/Footer.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import styled from "styled-components";
 
 export default function Footer() {
   return (
-    <>
-      Footer 입니다.
-    </>
+    <FooterWrapper>
+      <FooterItem>
+        Footer 입니다.
+      </FooterItem>
+    </FooterWrapper>
   )
 }
+
+const FooterWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const FooterItem = styled.div`
+  margin-top: auto;
+`
