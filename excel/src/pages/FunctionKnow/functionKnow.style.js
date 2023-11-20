3177078f9fd128503169e3eb9c2405465420877c
nastar <an1413@hs.ac.kr>
2023-11-20 13:28:52 +0900

    Feat: 함수별문제 기능 추가

--- excel/src/pages/FunctionKnow/functionKnow.style.js
+++ excel/src/pages/FunctionKnow/functionKnow.style.js
@@ -1,13 +1,78 @@
+// import styled from 'styled-components';
+
+// export const CardWrapper = styled.div`
+//   margin: 2rem auto;
+//   width: 1200px;
+//   display:flex;
+//   flex-wrap: wrap;
+// `;
+
+// export const CardTitle = styled.h1`
+//   display:block;
+// `
+
+// export const ListGroup = styled.div`
+//   /* display: flex; */
+//   margin-bottom: 1rem;
+// `
+
+// export const ListGroupItem = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   width: 200px;
+//   height: 100px;
+//   padding: 1rem;
+//   border-radius: 1rem;
+//   background-color: #fff;
+//   &:hover {
+//     cursor: pointer;
+//     color: #ddd;
+//     background-color: green;
+//     scale: 1.05;
+//   }
+// `
+
 import styled from 'styled-components';
 
-export const ListGroupe = styled.div`
-`
+export const CardWrapper = styled.div`
+  margin: 2rem auto;
+  width: 80%;
+  max-width: 1200px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
+
+export const CardTitle = styled.h1`
+  text-align: center;
+  width: 100%;
+  margin-bottom: 1rem;
+`;
+
+export const ListGroup = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
 
 export const ListGroupItem = styled.div`
-  margin: 20px;
+  width: 200px;
+  height: 100px;
+  margin: 1rem;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 1rem;
+  background-color: #fff;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease-in-out;
+
   &:hover {
     cursor: pointer;
-    color: #ddd;
+    color: #fff;
+    background-color: green;
+    transform: scale(1.05);
   }
-`
-
+`;
---
+++ excel/src/pages/FunctionProblem/FunctionProblem.jsx
@@ -0,0 +1,92 @@
+// FunctionProblem.js
+
+import React, { useState, useEffect } from 'react';
+import Swal from 'sweetalert2';
+import { ExpertWrapper, QuestionDiv, ExpertAnswer, ExpertHint, HintImage, QuestionH1, QuestionP, AnswerInput, AnswerButton, ExpertHintStrong, ButtonWrapper, AnswerForm } from '../Expert/expert.style';
+import { useNavigate } from 'react-router-dom';
+
+export const FunctionProblem = () => {
+  const navigate = useNavigate();
+  const selectedFunctionData = JSON.parse(localStorage.getItem('selectedFunctionData'));
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    if (!selectedFunctionData) {
+      // 선택된 함수에 대한 정보가 없으면 예외 처리 또는 홈으로 이동 등을 수행
+      Swal.fire({
+        title: '선택된 함수 정보가 없습니다.',
+        icon: 'error',
+        timer: 1500,
+      }).then(() => {
+        navigate('/');
+      });
+    }
+  }, [selectedFunctionData, navigate]);
+
+  const handleButton = () => {
+    // 정답 처리 로직
+    const expert_answer = selectedFunctionData.answer.toString();
+
+    if (inputValue.replace(/\s+/g, '') === expert_answer) {
+      // 정답과 입력값이 일치하면 성공 메시지 출력
+      Swal.fire({
+        title: '잘 하셨어요! 정답입니다.',
+        icon: 'success',
+        timer: 2000,
+      }).then(() => {
+        // 다음 문제로 이동 또는 홈으로 이동 등을 수행
+        navigate('/');
+      });
+    } else {
+      // 오답 메시지 출력
+      Swal.fire({
+        icon: 'error',
+        title: '틀렸습니다.',
+        text: '다시 한번 풀어보세요. 힌트가 도움이 될 것입니다.',
+      });
+      setInputValue('');
+    }
+  };
+
+  return (
+    <ExpertWrapper>
+      <div className="container">
+        <div className="row">
+          <QuestionDiv className="col col-sm-12 col-md-12 col-lg-6">
+            {/* 기존의 문제 출력 코드를 selectedFunctionData를 사용하여 수정 */}
+            <ButtonWrapper>
+              {/* 필요한 버튼 및 컴포넌트 추가 */}
+            </ButtonWrapper>
+            <QuestionH1>{selectedFunctionData.function} 함수 문제</QuestionH1>
+            <QuestionP>{selectedFunctionData.question}</QuestionP>
+            <AnswerForm>
+              <AnswerInput
+                type="text"
+                required
+                placeholder="여기에 정답을 입력해주세요."
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+              <AnswerButton type="button" onClick={handleButton}>
+                정답 입력
+              </AnswerButton>
+            </AnswerForm>
+          </QuestionDiv>
+          <div className="col col-sm-12 col-md-6 col-lg-6">
+            <div>
+              <ExpertAnswer>{/* 필요한 내용 추가 */}</ExpertAnswer>
+            </div>
+            <br />
+            <ExpertHint>
+              <ExpertHintStrong>{/* 필요한 내용 추가 */}</ExpertHintStrong>
+              <br />
+              <HintImage src={selectedFunctionData.sheet_photo} alt="힌트 이미지" />
+            </ExpertHint>
+          </div>
+        </div>
+      </div>
+    </ExpertWrapper>
+  );
+};
+
+export default FunctionProblem;
